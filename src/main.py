015a1611befb6c3344b519c87be16c09654f1461
Jack Moore <j@ck-m.co.uk>
2022-02-12 21:25:11 +0000

    Add sample for using a cache

--- src/main.py
+++ src/main.py
@@ -7,29 +7,45 @@
 ###############################
 import os
 import sys
+import pickle
 
 # Import additional files by saving them in this folder and importing
 # them relative to this file. e.g: from myFile import my_function
 
-def get_file_name_from_location(file_location):
-    # 
-    # Give the input file location, return the file name
-    # excluding the file type identifier
-    #  
-    return os.path.basename(file_location).split(".")[0]
+
+def do_with_cache(input_file_location, identifier, param):
+    #
+    # Sample method. This method will first check if the result
+    # is cached and use that if avaliable. Else it will do the
+    # work and then cache the results
+    #
+    input_file_name = os.path.basename(input_file_location).split(".")[0]
+    cache_file = os.path.join(
+        ".\cache", input_file_name + "_" + identifier + ".pkl")
+
+    if os.path.isfile(cache_file):
+        print(f"Using Cache for {identifier}")
+        with open(cache_file, "rb") as f:
+            res = pickle.load(f)
+    else:
+        # Do the work here
+        res = "Something..."
+        with open(cache_file, "wb") as f:
+            pickle.dump(res, f)
+
+    return res
 
 
 def run(file_location, output_location):
     print("Running Hash Code Entry")
     # Hash Code here :)
-    
+
     # ----- SAMPLE: Read the input file -----
     # input_file = open(file_location, "r")
     # for line in input_file:
     #     # Do Something
     # input_file.close()
     # ---------------------------------------
-
 
     # ----- SAMPLE: Write out results -----
     # output_file = open(output_location, "w")
